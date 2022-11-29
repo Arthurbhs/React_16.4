f1f34c546c66f56e0e92a9660a7812963329d683
Arthurbhs <100363252+Arthurbhs@users.noreply.github.com>
2022-11-29 10:15:11 -0300

    Componente classe 2

--- exercicios/src/component/Saudation.jsx
+++ exercicios/src/component/Saudation.jsx
@@ -1,21 +1,33 @@
 import React, {Component} from 'react'
 
 export default class Saudation extends Component {
-state = {
-    type: 'fala',
-    name: 'pedro'
+
+    state = {
+        type: 'fala',
+        name: 'pedro'
+    }
+constructor(props){
+    super(props)
+
+this.setType = this.setType.bind(this)
+
+
 }
     setType(e) {
        this.setState({type: e.target.value})
     }
+
+    setName(e) {
+        this.setState({type: e.target.value})
+     }
 
 render(){
     const {type, name} = this.props
     return (<div>
 <h1>{type} {name}</h1>
 <hr/>
-<input type="text" placeholder='tipo....' value={type} onChange={è => this.setType(e)} />
-<input type="text" placeholder='nome...' value={name} onChange={è => this.setType(e)} />
+<input type="text" placeholder='tipo....' value={type} onChange={this.setType} />
+<input type="text" placeholder='nome...' value={name} onChange={ e => this.setName()} />
 
 
 </div>
